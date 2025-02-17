16e42f23ad22de119d66517704d684b8bf5a2170
S16-sakthi <logeshlogesh212@gmail.com>
2025-02-17 16:00:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function submitData(){
+var name =document.getElementById('name').value
+var dob =document.getElementById('dob').value
+console.log(name+""+dob)
+var output=document.getElementById('output');
+output.innerText(name+""+dob)
+`student name: $name
+
+}
